545bc5c77ee2a8dea65e597b67b8767c1bff521e
Nabin <nabinkumar031@gmail.com>
2022-01-23 13:31:12 +0530

    Initial Commit

---
+++ src/main/java/InvoiceSummary.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class InvoiceSummary {
+
+    private final int numberOfRides;
+    private final double totalFare;
+    private final double averageFare;
+
+    public InvoiceSummary(int numberOfRides, double fare) {
+        this.numberOfRides = numberOfRides;
+        this.totalFare=fare;
+        this.averageFare = totalFare/this.numberOfRides;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return numberOfRides == that.numberOfRides &&
+                Double.compare(that.totalFare, totalFare) == 0 &&
+                Double.compare(that.averageFare, averageFare) == 0;
+    }
+
+
+}
